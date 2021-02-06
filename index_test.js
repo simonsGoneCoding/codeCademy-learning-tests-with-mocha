775419050a9e0d44f62930c76b3e4d35a23aa96f
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-06 19:23:35 +0000

    Teardown - reset any conditions that were changed during the test.

--- index_test.js
+++ index_test.js
@@ -1,4 +1,5 @@
 const assert = require("assert");
+const fs = require("fs");
 
 //describe and it functions
 const arr = [1, 5, 7, 9, 0];
@@ -43,5 +44,24 @@ describe(".pop", () => {
 
     // Verify
     assert.ok(popped === knightString);
+  });
+});
+
+//Teardown - reset any conditions that were changed during the test
+describe("appendFileSync", () => {
+  it("writes a string to text file at given path name", () => {
+    // Setup
+    const path = "./message.txt";
+    const str = "Hello Node.js";
+
+    // Exercise: write to file
+    fs.appendFileSync(path, str);
+
+    // Verify: compare file contents to string
+    const contents = fs.readFileSync(path);
+    assert.ok(contents.toString() === str);
+
+    // Teardown: delete path
+    fs.unlinkSync(path);
   });
 });
