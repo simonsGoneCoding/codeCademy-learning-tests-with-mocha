a7410b0a6ad2f8e6827dadf4fadcc5b0e0886a64
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-06 20:38:38 +0000

    Hooks - afterEach (runs after each it block is execcuted).

--- index_test.js
+++ index_test.js
@@ -65,3 +65,26 @@ describe("appendFileSync", () => {
     fs.unlinkSync(path);
   });
 });
+
+//HOOKS
+describe("appendFileSync", () => {
+  const path = "./message.txt";
+
+  //HOOK - runs after each it block is executed.
+  afterEach(() => {
+    // Teardown: delete path
+    fs.unlinkSync(path);
+  });
+
+  it("writes a string to text file at given path name", () => {
+    // Setup
+    const str = "Hello Node.js";
+
+    // Exercise: write to file
+    fs.appendFileSync(path, str);
+
+    // Verify: compare file contents to string
+    const contents = fs.readileSync(path);
+    assert.ok(contents.toString() === str);
+  });
+});
