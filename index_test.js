4101b30872138d51b288b886b137d233f058501a
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-06 18:40:26 +0000

    Learn assert.

--- index_test.js
+++ index_test.js
@@ -1,14 +1,24 @@
+const assert = require("assert");
+
+//describe and it functions
 const arr = [1, 5, 7, 9, 0];
 
 describe("Math", () => {
   describe(".max", () => {
     it("returns the argument with the highest value", () => {
-      Math.max([...arr]);
+      Math.max(...arr);
     });
   });
   describe(".min", () => {
     it("returns the argument with the lowest value", () => {
-      Math.min([...arr]);
+      Math.min(...arr);
     });
+  });
+});
+
+//assert
+describe("+", () => {
+  it("returns the sum of its args", () => {
+    assert(1 + 4 === 5);
   });
 });
